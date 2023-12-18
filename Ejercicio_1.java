747f936fd0e84c0b286113155f5ef932962f8283
Matiterativo <129184914+Matiterativo@users.noreply.github.com>
2023-12-18 18:49:17 -0300

    Fix tabs

--- Ejercicio_1.java
+++ Ejercicio_1.java
@@ -7,38 +7,38 @@ public class Ejercicio_1 {
 	public static void main(String[] args) {
 		
 		final String TEXTO_INICIO_PROGRAMA = " =========================================================== \n" +
-				 							 " \u001B[1mINGRESO AL SISTEMA PROTEGIDO\u001B[0m \n" +
-				 							 " =========================================================== \n";
+				 		     " \u001B[1mINGRESO AL SISTEMA PROTEGIDO\u001B[0m \n" +
+				 		     " =========================================================== \n";
 		final String MENSAJE_INGRESO_CORRECTO ="------------------------- \n" + "Bienvenido al sistema";
 		final String MENSAJE_CREDENCIALES_INCORRECTAS ="\n Credenciales incorrectas";
 		final String MENSAJE_CANTIDAD_INTENTOS ="\n Intento: %d / %d \n--------------------------\n";
 		
 		String usuarioSistema = "javabootcamp";
-        String passwordSistema = "password2023";
-        String usuarioIngresado;
-        String passwordIngresado;
-        int maximosIntentos = 3;
-        int intentos =0;
+        	String passwordSistema = "password2023";
+        	String usuarioIngresado;
+        	String passwordIngresado;
+        	int maximosIntentos = 3;
+        	int intentos =0;
         
-        Scanner scanner = new Scanner(System.in);
+        	Scanner scanner = new Scanner(System.in);
         
-        System.out.println(TEXTO_INICIO_PROGRAMA);
+        	System.out.println(TEXTO_INICIO_PROGRAMA);
 		
 		do {
 			
-	        System.out.println("Ingrese el usuario: ");
-	        usuarioIngresado = scanner.nextLine();
-	        System.out.println("Ingrese la clave: ");
-	        passwordIngresado = scanner.nextLine();
-	        intentos++;
+		        System.out.println("Ingrese el usuario: ");
+		        usuarioIngresado = scanner.nextLine();
+		        System.out.println("Ingrese la clave: ");
+		        passwordIngresado = scanner.nextLine();
+		        intentos++;
 	        
-	        if (usuarioSistema.equals(usuarioIngresado) && passwordSistema.equals(passwordIngresado)) {
-	        	System.out.println(MENSAJE_INGRESO_CORRECTO);
-	        	break;
-	        }else {
-	        	System.out.println(MENSAJE_CREDENCIALES_INCORRECTAS);
-	        	System.out.println(String.format(MENSAJE_CANTIDAD_INTENTOS, intentos, maximosIntentos));
-	        }
+	        	if (usuarioSistema.equals(usuarioIngresado) && passwordSistema.equals(passwordIngresado)) {
+	        		System.out.println(MENSAJE_INGRESO_CORRECTO);
+	        		break;
+	        	}else {
+	        		System.out.println(MENSAJE_CREDENCIALES_INCORRECTAS);
+	        		System.out.println(String.format(MENSAJE_CANTIDAD_INTENTOS, intentos, maximosIntentos));
+	        	}
 			
 		} while (intentos < maximosIntentos);
 		
